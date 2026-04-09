f20260e9e9592f2b988573d9db7085af0dc6042c
Umesh Joshi <slackuj@gmail.com>
2026-04-09 07:37:49 +0545

    update

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,5 +1,9 @@
-import {decodeToken, getAccessToken, getRefreshToken} from "../../utils/tokenUtil.ts";
+import {decodeToken, getAccessToken, getRefreshToken, setAccessToken, setRefreshToken} from "../../utils/tokenUtil.ts";
 import {createSlice} from "@reduxjs/toolkit";
+import {apiSlice} from "../../api/apiSlice.ts";
+import type {LoginResponseData, UserLoginRequest} from "../../types/auth.ts";
+import {config} from "../../config.ts";
+import type {ApiResponse} from "../../types/response.ts";
 
 export interface AuthState {
     id: string | undefined;
@@ -23,12 +27,54 @@ const initialState: AuthState = {
     permissions: decodedAccessToken?.permissions,
     isAuthenticated: !!accessToken
 };
-
+0
 const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {},
-    extraReducers: () => {},
+    extraReducers: (builder) => {
+        builder.addMatcher(
+            authApiSlice.endpoints.login.matchFulfilled, (state, action) => {
+                const { accessToken, refreshToken } = action.payload;
+                if ( accessToken && refreshToken ) {
+                    state.accessToken = accessToken;
+                    state.refreshToken = refreshToken;
+
+                    // consider performing non-state update tasks outside the reducer ??? such as in onQueryStarted/Updated method ... also can transform response to match the required payload !?
+                    setAccessToken(accessToken);
+                    setRefreshToken(refreshToken);
+
+                    const decodedAccessToken = decodeToken(accessToken);
+                    state.id = decodedAccessToken?.id;
+                    state.roles = decodedAccessToken?.roles;
+                    state.permissions = decodedAccessToken?.permissions;
+                    state.isAuthenticated = true;
+                }
+            }
+        );
+    },
 });
 
 export default authSlice.reducer;
+
+/***********************************************/
+/*** INJECTING AUTH ENDPOINTS INTO APISLICE  ***/
+/***********************************************/
+
+const authApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        login: builder.mutation< LoginResponseData,UserLoginRequest>({
+            query: (userLoginRequestData) => ({
+                url: config.endpoints.login,
+                method: "POST",
+                body: userLoginRequestData
+            }),
+            // notice if this is equivalent to response.data.data !? ---> i.e is `return response.data` handled automatically by RTK Query as specified ?
+            transformResponse: (response: ApiResponse<LoginResponseData>) => response.data,
+        }),
+    }),
+});
+
+export const {
+    useLoginMutation,
+} = authApiSlice;
